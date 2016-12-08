cc54a752f548f38789d9eee421f3d11314d646c1
Jan Jecmen <jan.jecmen@gmail.com>
2016-12-08 14:20:49 +0100

    WIP backward driver

--- rir/src/code/analysis.h
+++ rir/src/code/analysis.h
@@ -283,4 +283,324 @@ static inline bool isSafeBuiltin(int i) {
     }
     return false;
 }
+
+
+/** Backward analysis driver
+ */
+template <typename ASTATE>
+class BackwardAnalysis : public Analysis {
+  public:
+    void invalidate() override {
+        Analysis::invalidate();
+        delete currentState_;
+        delete initialState_;
+        delete finalState_;
+        initialState_ = nullptr;
+        currentState_ = nullptr;
+        finalState_ = nullptr;
+        for (auto& s : mergePoints_)
+            delete s.second;
+        mergePoints_.clear();
+    }
+
+    void print() override {}
+
+  protected:
+    BackwardAnalysis() {}
+
+    ASTATE & current() {
+        return * reinterpret_cast<ASTATE *>(currentState_);
+    }
+
+    virtual Dispatcher & dispatcher() = 0;
+
+    virtual ASTATE * initialState() {
+        return new ASTATE();
+    }
+
+    void doAnalyze() override {
+        computeBackwardCF();
+        initialState_ = initialState();
+        Dispatcher & d = dispatcher();
+
+        while (not q_.empty()) {
+            currentIns_ = q_.front();
+            q_.pop_front();
+
+            while (true) {
+                BC cur = *currentIns_;
+
+                // start of new path
+                if (cur.isReturn()) {
+                    assert(currentState_ == nullptr);
+                    currentState_ = initialState_->clone();
+                }
+
+                // merging
+                else if (isBasicBlockEnd(currentIns_)) {
+
+                    // if state not stored, store copy of incoming
+                    State * & stored = mergePoints_[currentIns_];  // first call to [] initializes to nullptr
+                    if (stored == nullptr) {
+                        assert(currentState_ != nullptr);
+                        stored = currentState_->clone();
+                    } else {
+                        // if incoming not present, copy stored
+                        if (currentState_ == nullptr) {
+                            currentState_ = stored->clone();
+                        // otherwise merge incoming with stored
+                        } else if (stored->mergeWith(currentState_)) {
+                            delete currentState_;
+                            currentState_ = stored->clone();
+                        // and terminate current branch if there is no need to continue
+                        } else {
+                            delete currentState_;
+                            currentState_ = nullptr;
+                            break;
+                        }
+                    }
+                }
+
+                // user dispatch method
+                d.dispatch(currentIns_);
+
+                // endpoint is begining
+                if (currentIns_ == code_->begin()) {
+                    if (finalState_ == nullptr) {
+                        finalState_ = currentState_;
+                    } else {
+                        finalState_->mergeWith(currentState_);
+                        delete currentState_;
+                    }
+                    currentState_ = nullptr;
+                    break;
+                }
+
+                // add jumps, terminate finished branches
+                if (isBasicBlockStart(currentIns_)) {
+
+                    // possible multiple paths lead to this instruction
+                    if (cur.is(BC_t::label)) {
+
+                        // prev is unconditional jump -> terminate branch
+
+
+                        // merge all origin points to this label and continue to prev
+
+                        for (auto& o : jumpOrigins_[cur.immediate.offset]) {
+
+
+
+
+                        }
+
+
+
+
+                    }
+
+                    // else: had to come from previous instruction
+                    // (unless prev is unconditional jump, then this would be dead code!!)
+                    // -> merge postponed to previous instruction
+
+                }
+
+                // FIXME: sequences JUMP + LABEL cause merge twice... !
+
+                if (cur.is(BC_t::br_)) {
+                    Label l = cur.immediate.offset;
+                    if (shouldJump(l)) {
+                        q_.push_front(code_->target(cur));
+                    }
+                    delete currentState_;
+                    currentState_ = nullptr;
+                    break;
+                } else if (cur.isJmp()) {
+                    Label l = cur.immediate.offset;
+                    if (shouldJump(l)) {
+                        q_.push_front(code_->target(cur));
+                    }
+                }
+
+
+                // move to the previous instruction
+                --currentIns_;
+            }
+        }
+    }
+
+    State * initialState_ = nullptr;  // multiple initial states for all returns?? (but they are all the same)
+    State * currentState_ = nullptr;
+    State * finalState_ = nullptr;
+    CodeEditor::Iterator currentIns_;
+
+    std::unordered_map<CodeEditor::Iterator, State *> mergePoints_;
+
+
+private:
+    bool shouldJump(size_t label) {
+        State * & stored = mergePoints_[label];
+        if (stored == nullptr) {
+            stored = currentState_->clone();
+            return true;
+        } else {
+            return stored->mergeWith(currentState_);
+        }
+    }
+
+    bool isBasicBlockStart(CodeEditor::Iterator ins) const {
+        // Used to decide if we should merge states
+
+        BC cur = *ins;
+
+        // labels are starts
+        if (cur.is(BC_t::label)) return true;
+
+        // first instruction handled separately
+        if (ins == code_->begin()) return false;
+
+        // jump plus whatever means whatever is start
+        BC prev = *(ins - 1);
+        if (prev.isJmp()) return true;
+
+        return false;
+    }
+
+    bool isBasicBlockEnd(CodeEditor::Iterator ins) const {
+        // Used to decide if we should merge states
+
+        BC cur = *ins;
+
+        // jumps are ends
+        if (cur.isJmp()) return true;
+
+        // last instruction and returns handled separately, i.e. no merging for them
+        if (ins == code_->end() || cur.isReturn()) return false;
+
+        // whatever plus label means whatever is end
+        BC next = *(ins + 1);
+        if (next.is(BC_t::label)) return true;
+
+        return false;
+    }
+
+    void computeBackwardCF() {
+        // find for all labels the jumps that target them
+        // also add all returns to the working list
+
+        jumpOrigins_.resize(code_->numLabels());
+
+        int mpCount = 0;
+
+        auto it = code_->begin(), end = code_->end();
+        while (it != end) {
+            BC ins = *it;
+
+            // remember jump targets
+            if (ins.isJmp()) {
+                Label l = ins.immediate.offset;
+                jumpOrigins_[l].push_back(it);
+            }
+
+            // count merge points
+            if (isBasicBlockEnd(it)) {
+                ++mpCount;
+            }
+
+            // add entry point to working list
+            if (ins.isReturn()) {
+                q_.push_front(it);
+            }
+
+            ++it;
+        }
+
+        mergePoints_.reserve(mpCount);
+    }
+
+    std::vector<std::vector<CodeEditor::Iterator>> jumpOrigins_;
+    std::deque<CodeEditor::Iterator> q_;
+};
+
+template<typename ASTATE>
+class BackwardAnalysisFinal : public BackwardAnalysis<ASTATE> {
+    using BackwardAnalysis<ASTATE>::finalState_;
+public:
+    ASTATE const & finalState() {
+        return * reinterpret_cast<ASTATE *>(finalState_);
+    }
+};
+
+template<typename ASTATE>
+class BackwardAnalysisIns : public BackwardAnalysisFinal<ASTATE> {
+    using BackwardAnalysis<ASTATE>::currentIns_;
+    using BackwardAnalysis<ASTATE>::currentState_;
+    using BackwardAnalysis<ASTATE>::initialState_;
+    using BackwardAnalysis<ASTATE>::dispatcher;
+    using BackwardAnalysis<ASTATE>::mergePoints_;
+protected:
+    using BackwardAnalysis<ASTATE>::current;
+    using BackwardAnalysis<ASTATE>::code_;
+public:
+  ASTATE const& operator[](CodeEditor::Iterator ins) {
+        if (ins != currentIns_)
+            seek(ins);
+        return current();
+    }
+    ASTATE const& operator[](CodeEditor::Cursor cur) {
+        auto ins = cur.asItr();
+        if (ins != currentIns_)
+            seek(ins);
+        return current();
+    }
+
+protected:
+
+    void doAnalyze() override {
+        BackwardAnalysis<ASTATE>::doAnalyze();
+        initializeCache();
+    }
+
+    void initializeCache() {
+//        currentState_ = initialState_->clone();
+//        currentIns_ = code_->begin();
+      //  assert(false && "Not yet implemented.");
+    }
+
+    void advance() {
+//        dispatcher().dispatch(currentIns_);
+//        ++currentIns_;
+//        // if the cached instruction is label, dispose of the state and create a copy of the fixpoint
+//        if ((*currentIns_).is(BC_t::label)) {
+//            auto fixpoint = mergePoints_[(*currentIns_).immediate.offset];
+//            // if we reach dead code there is no merge state available
+//            if (fixpoint) {
+//                delete currentState_;
+//                currentState_ = fixpoint->clone();
+//            }
+//        }
+        assert(false && "Not yet implemented.");
+    }
+
+    void seek(CodeEditor::Iterator ins) {
+//        while (currentIns_ != code_->end()) {
+//            if (currentIns_ == ins)
+//                return;
+//            if ((*currentIns_).isReturn())
+//                break;
+//            // advance the state using dispatcher
+//            advance();
+//        }
+//        // if we haven't found it, let's start over
+//        initializeCache();
+//        while (currentIns_ != code_->end()) {
+//            if (currentIns_ == ins)
+//                return;
+//            advance();
+//        }
+//        assert(false and "not reachable");
+        assert(false && "Not yet implemented.");
+    }
+};
+
 }
